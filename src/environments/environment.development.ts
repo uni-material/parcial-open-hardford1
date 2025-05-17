f5f2f200a5d300a8c09c42dd4d4bea1cf764fdbf
RominaMaita <u202213765@upc.edu.pe>
2025-05-16 23:39:06 -0500

    feat: add db.json data.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  examinersEndpointPath: '/examiners',
+  patientsEndpointPath: '/patients',
+  mentalStateExamsEndpointPath: '/mental-state-exams'
+
+};
